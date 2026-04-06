842d7d5c19f261775696beece9d0ac642c1c6af5
Minh Tam <trantam2004hocjava@gmail.com>
2026-04-06 10:56:14 +0700

    Update

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
-from . import utils
-
 setup(
     name="kltn_utils",  # Tên thư viện của bạn
     version="0.1.0",  # Phiên bản thư viện
     packages=find_packages(),  # Tìm các package trong thư mục hiện tại
-    install_requires=utils.read_req(),  # Các thư viện phụ thuộc (nếu có)
+    install_requires=[
+        "git+https://github.com/openai/CLIP.git"
+    ],  # Các thư viện phụ thuộc (nếu có)
 )
