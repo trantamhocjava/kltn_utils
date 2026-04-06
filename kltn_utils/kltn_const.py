ccfb429b96b84ab2af9363d39903b6a33c9619f8
Minh Tam <trantam2004hocjava@gmail.com>
2026-04-06 10:26:48 +0700

    Update

--- kltn_utils/kltn_const.py
+++ kltn_utils/kltn_const.py
@@ -1,10 +1,12 @@
 import torch
-from torchvision.transforms import v2
+from torchvision.transforms import InterpolationMode, v2
 
 SEEDING = 42
 METRIC_MAX = ("val_c_acc_overall", "val_c_acc", "val_y_acc", "val_y_bmac")
 PREPROCESS_LIST = [
-    v2.Resize(size=224, interpolation="bicubic", max_size=None, antialias=True),
+    v2.Resize(
+        size=224, interpolation=InterpolationMode.BICUBIC, max_size=None, antialias=True
+    ),
     v2.CenterCrop(size=(224, 224)),
     v2.ToDtype(torch.float32, scale=True),
     v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
