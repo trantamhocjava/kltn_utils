e459c348ff7bb70224c275ca87e309b598c537a3
Minh Tam <trantam2004hocjava@gmail.com>
2026-04-15 21:51:39 +0700

    Update

--- kltn_utils/kltn_utils.py
+++ kltn_utils/kltn_utils.py
@@ -208,19 +208,23 @@ def unfreeze_module(m):
 
 
 def get_img_feat_from_clip_model(clip_model, clip_model_name, img):
-    if clip_model_name in kltn_const.CLIP_MODEL_FROM_OPENAI:
-        img_feat = clip_model.encode_image(img)
-    elif clip_model_name in kltn_const.CLIP_MODEL_FROM_HF_HUB:
-        img_feat = clip_model(img, None)[0]
+    clip_model.eval()
+    with torch.no_grad():
+        if clip_model_name in kltn_const.CLIP_MODEL_FROM_OPENAI:
+            img_feat = clip_model.encode_image(img)
+        elif clip_model_name in kltn_const.CLIP_MODEL_FROM_HF_HUB:
+            img_feat = clip_model(img, None)[0]
 
     return img_feat
 
 
 def get_concept_feat_from_clip_model(clip_model, clip_model_name, concept_token):
-    if clip_model_name in kltn_const.CLIP_MODEL_FROM_OPENAI:
-        concept_feat = clip_model.encode_text(concept_token)
-    elif clip_model_name in kltn_const.CLIP_MODEL_FROM_HF_HUB:
-        concept_feat = clip_model(None, concept_token)[1]
+    clip_model.eval()
+    with torch.no_grad():
+        if clip_model_name in kltn_const.CLIP_MODEL_FROM_OPENAI:
+            concept_feat = clip_model.encode_text(concept_token)
+        elif clip_model_name in kltn_const.CLIP_MODEL_FROM_HF_HUB:
+            concept_feat = clip_model(None, concept_token)[1]
 
     return concept_feat
 
