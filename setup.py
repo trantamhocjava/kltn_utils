640fdd1a2972fa60310ac09c110146743c11e65f
Minh Tam <trantam2004hocjava@gmail.com>
2026-04-16 13:16:52 +0700

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="kltn_utils",  # Tên thư viện của bạn
-    version="0.1.0",  # Phiên bản thư viện
+    version="0.1.1",  # Phiên bản thư viện
     packages=find_packages(),  # Tìm các package trong thư mục hiện tại
     install_requires=[],  # Các thư viện phụ thuộc (nếu có)
 )
