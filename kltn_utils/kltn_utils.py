e2760443a335ff7c43b4443328f93d9a13453e89
Minh Tam <trantam2004hocjava@gmail.com>
2026-04-21 08:53:34 +0700

    Update

--- kltn_utils/kltn_utils.py
+++ kltn_utils/kltn_utils.py
@@ -2,6 +2,7 @@ import csv
 import gzip
 import json
 import os
+import random
 import shutil
 
 import clip
@@ -272,3 +273,36 @@ def show_img(
 
     print(f"resolution: {img_width} x {img_height}")
     print(f"file format: {file_format}")
+
+
+def random_sublist(src_list, num, replace=False):
+    if replace:
+        sub_list = random.choices(src_list, k=num)
+    else:
+        sub_list = random.sample(src_list, num)
+
+    return sub_list
+
+
+def show_images(image_paths, m, n, figsize=(12, 8)):
+    fig, axes = plt.subplots(m, n, figsize=figsize)
+
+    # Trường hợp m*n = 1 thì axes không phải mảng
+    if m == 1 and n == 1:
+        axes = [[axes]]
+    elif m == 1:
+        axes = [axes]
+    elif n == 1:
+        axes = [[ax] for ax in axes]
+
+    for i in range(len(image_paths)):
+        row = i // n
+        col = i % n
+        ax = axes[row][col]
+
+        img = Image.open(image_paths[i])
+        ax.imshow(img)
+        ax.axis("off")
+
+    plt.tight_layout()
+    plt.show()
