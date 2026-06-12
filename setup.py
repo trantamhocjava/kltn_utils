3e2f38f5d31c9ffd126b476cbda872da8685cb70
Minh Tam <trantam2004hocjava@gmail.com>
2026-06-12 09:36:08 +0700

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,19 @@ from setuptools import find_packages, setup
 
 setup(
     name="kltn_utils",  # Tên thư viện của bạn
-    version="0.1.72",  # Phiên bản thư viện
+    version="0.1.73",  # Phiên bản thư viện
     packages=find_packages(),  # Tìm các package trong thư mục hiện tại
     install_requires=[],  # Các thư viện phụ thuộc (nếu có)
+    include_package_data=True,
+    package_data={
+        "kltn_utils": [
+            "cbm/data/isic2018/*.json",
+            "cbm/data/bccd/*.json",
+            "cbm/data/busi/*.json",
+            "cbm/data/dtr/*.json",
+            "cbm/data/idrid/*.json",
+            "cbm/data/lcc/*.json",
+            "cbm/data/nct/*.json",
+        ],
+    },
 )
