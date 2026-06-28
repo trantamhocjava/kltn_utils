e4ab026769bd50b6e4f7686a12b3dd74c028e721
Minh Tam <trantam2004hocjava@gmail.com>
2026-06-28 12:49:13 +0700

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="kltn_utils",  # Tên thư viện của bạn
-    version="0.1.108",  # Phiên bản thư viện
+    version="0.1.109",  # Phiên bản thư viện
     packages=find_packages(),  # Tìm các package trong thư mục hiện tại
     install_requires=[
         "transformers",
@@ -23,6 +23,7 @@ setup(
             "cbm/data/lcc/*.json",
             "cbm/data/nct/*.json",
             # File cấu hình PMC-CLIP
+            "third_party/pmc_clip/*.gz",
             "third_party/pmc_clip/model_configs/*.json",
         ],
     },
