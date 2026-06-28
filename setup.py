03243f34c26a552c2f30330ef2e75947830bc51c
Minh Tam <trantam2004hocjava@gmail.com>
2026-06-28 12:43:59 +0700

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="kltn_utils",  # Tên thư viện của bạn
-    version="0.1.107",  # Phiên bản thư viện
+    version="0.1.108",  # Phiên bản thư viện
     packages=find_packages(),  # Tìm các package trong thư mục hiện tại
     install_requires=[
         "transformers",
